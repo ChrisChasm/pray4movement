0058c568f0723cc0a815850a0451312e8f003d86
Chris Chasm <chris@chasm.solutions>
2020-10-06 12:30:25 -0600

    Add svg support

--- functions.php
+++ functions.php
@@ -1,5 +1,11 @@
 <?php
 
+function cc_mime_types($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
 register_activation_hook( __FILE__, [ 'Pray4Movement', 'pray4movement_activation' ] );
 function pray4movement_activation(){
     if ( empty( get_page_by_title('Home', ARRAY_A, 'page' ) ) ) {
